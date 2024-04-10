4833605653561777462945c6d173266da6ab5f9c
mercynjambi <njambim695@gmail.com>
2024-04-10 12:17:29 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+// Remove the <main> with id 'main'
+const mainElement = document.querySelector('main#main');
+mainElement.remove();
+
+// Create a new <h1> element and assign it to the variable newHeader
+const newHeader = document.createElement('h1');
+
+// Set the id of the <h1> element to 'victory'
+newHeader.id = 'victory';
+
+// Set the text content of the <h1> element to "YOUR-NAME is the champion"
+newHeader.textContent = " Njambi is the champion"; // Replace YOUR-NAME with your desired text
